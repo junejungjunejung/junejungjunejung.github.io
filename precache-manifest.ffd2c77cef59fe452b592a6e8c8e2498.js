976165ce1702e1f86d3fd052059ba03d96837e69
unknown <junejungjunejung@gmail.com>
2020-11-18 14:44:00 -0800

    footer layout styling

---
+++ precache-manifest.ffd2c77cef59fe452b592a6e8c8e2498.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2a631c9df6d20853e4f4effb209941e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "649e24bfe498f00f5225",
+    "url": "/static/css/main.0a0e2ddf.chunk.css"
+  },
+  {
+    "revision": "8d527b8e4a5cf6d0cc4c",
+    "url": "/static/js/2.1764fe8c.chunk.js"
+  },
+  {
+    "revision": "5f8be6edcb631ea3c834b8459dc2c264",
+    "url": "/static/js/2.1764fe8c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "649e24bfe498f00f5225",
+    "url": "/static/js/main.a0f848e6.chunk.js"
+  },
+  {
+    "revision": "10a3591ef24d3528a7ad",
+    "url": "/static/js/runtime-main.8bcec1ae.js"
+  },
+  {
+    "revision": "c76df2dd9f20cf509e54f0c79e45e717",
+    "url": "/static/media/cat-solid.c76df2dd.svg"
+  },
+  {
+    "revision": "07aef9a260c1fa917b402f9a49570a5f",
+    "url": "/static/media/cloud-sun-solid.07aef9a2.svg"
+  },
+  {
+    "revision": "da3a9ed4399e0dc38e09a9d494a62f28",
+    "url": "/static/media/google-brands.da3a9ed4.svg"
+  },
+  {
+    "revision": "1fd4395d1886024be4b2989d1845e47d",
+    "url": "/static/media/junejung-logo.1fd4395d.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.42320911988878801cbd90dedce84a99.js"
+  "/precache-manifest.ffd2c77cef59fe452b592a6e8c8e2498.js"
 );
 
 self.addEventListener('message', (event) => {
